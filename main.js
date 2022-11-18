39f2ccb8a10e079399d17739a687836784a57071
zhangbinbbb <2362016211@qq.com>
2022-11-18 20:56:38 +0800

    添加新内容

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+// 按需导入 $http 对象
+import { $http } from '@escook/request-miniprogram'
+
+// 在 uni-app 项目中，可以把 $http 挂载到 uni 顶级对象之上，方便全局调用
+$http.baseUrl = 'http://127.0.0.1'
+// 请求开始之前做一些事情
+$http.beforeRequest = function (options) {
+  // do somethimg...
+  uni.showLoading({
+      title: '数据加载中...',
+  })
+}
+uni.$http = $http
 
 // #ifndef VUE3
 import Vue from 'vue'
